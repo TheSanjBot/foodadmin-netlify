41c73494efe629159f3cf812ea8e13edc007c516
Sanjay H <69153982+TheSanjBot@users.noreply.github.com>
2025-07-31 00:44:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Menubar from './components/Menubar/Menubar.jsx';
+import { Routes, Route } from 'react-router-dom';
+import AddFood from './pages/AddFood/AddFood.jsx';
+import ListFood from './pages/ListFood/ListFood.jsx';
+import Orders from './pages/Orders/Orders.jsx';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+
+
+
+
+
+const App = () => {
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  }
+  return (
+    <div className="d-flex" id="wrapper">
+            
+            <Sidebar sidebarVisible={sidebarVisible}/>
+            
+            <div id="page-content-wrapper">
+                
+                <Menubar toggleSidebar={toggleSidebar} />
+                <ToastContainer />
+                
+                <div className="container-fluid">
+                    <Routes>
+                      <Route path='/add' element={<AddFood />} />
+                      <Route path='/list' element={<ListFood />} />
+                      <Route path='/orders' element={<Orders />} />
+                      <Route path='/' element={<ListFood />} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default App;
